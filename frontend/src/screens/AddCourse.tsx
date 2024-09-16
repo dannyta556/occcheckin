40e9d9d3771042cf165c81c3a277ec849f8208bf
dannyta556 <dannyta556@gmail.com>
2024-09-16 16:50:54 -0700

    Frontend made for AddCourse and ViewStudents

--- frontend/src/screens/AddCourse.tsx
+++ frontend/src/screens/AddCourse.tsx
@@ -1,8 +1,62 @@
 import SearchPage from '../components/SearchPage';
+import { ListGroup } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import { Button, InputGroup } from 'react-bootstrap';
+
+const courseList = [
+  'Math 100',
+  'Math 104',
+  'Math 115',
+  'Math 120',
+  'Math 140',
+  'Math 170',
+  'Math 180',
+  'Math 185',
+  'Math 280',
+  'Math 285',
+];
 function AddCourseScreen() {
+  // useEffect pull courseList on page load and update
+  type ButtonEvent = React.MouseEvent<HTMLFormElement>;
+  const submitHandler = (e: ButtonEvent) => {
+    e.preventDefault();
+    // check if id exists
+    let exists = false;
+    if (exists) {
+      // show check-in and check-out buttons
+    } else {
+      // show error
+    }
+  };
   return (
-    <div>
-      <SearchPage title="Add Course" altpage="admin" />
+    <div className="center-box">
+      <SearchPage title="Course List" altpage="admin" />
+
+      <ListGroup className="center-box">
+        {courseList.map((course) => {
+          return (
+            <ListGroup.Item key={course} className="center-text">
+              {course}
+            </ListGroup.Item>
+          );
+        })}
+      </ListGroup>
+      <Form className="pad-top-lg" onSubmit={submitHandler}>
+        <InputGroup className="center-box">
+          <FormControl
+            className="input-box"
+            type="text"
+            name="q"
+            id="q"
+            placeholder="Enter Course"
+            aria-label="Search Student"
+          ></FormControl>
+          <Button variant="outline-primary" type="submit" id="button-search">
+            Add
+          </Button>
+        </InputGroup>
+      </Form>
     </div>
   );
 }
--- frontend/src/screens/ViewStudentsScreen.tsx
+++ frontend/src/screens/ViewStudentsScreen.tsx
@@ -1,8 +1,88 @@
 import SearchPage from '../components/SearchPage';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+
+const exampleStudentList = [
+  {
+    LastName: 'Doe',
+    FirstName: 'John',
+    id: 'C012839231',
+    MathLvl: 'Math 185',
+    TotalHrs: '40 hrs 32 min',
+  },
+  {
+    LastName: 'James',
+    FirstName: 'Mary',
+    id: 'C23489134',
+    MathLvl: 'Math 120',
+    TotalHrs: '8 hrs 24 min',
+  },
+  {
+    LastName: 'Korpela',
+    FirstName: 'Jukka',
+    id: 'C21482344',
+    MathLvl: 'Math 170',
+    TotalHrs: '13 hrs 12 min',
+  },
+  {
+    LastName: 'Balan',
+    FirstName: 'Pranav',
+    id: 'C23894135',
+    MathLvl: 'Math 120',
+    TotalHrs: '2 hrs 7 min',
+  },
+];
+
+const semesters = ['Fall 2024', 'Spring 2023', 'Fall 2023'];
+
 function ViewStudentsScreen() {
   return (
     <div>
       <SearchPage title="View Students" altpage="admin" />
+      <div className="center-content">
+        <div className="table-options">
+          <select name="fall 2024" id="semester">
+            {semesters.map((semester) => {
+              return (
+                <option key={semester} value={semester}>
+                  {semester}
+                </option>
+              );
+            })}
+          </select>
+          <Button className="align-item-right btn-export">Export</Button>
+        </div>
+
+        <Table className="student-table" striped bordered hover>
+          <thead>
+            <tr>
+              <th>Last Name</th>
+              <th>First Name</th>
+              <th>id</th>
+              <th>Math Level</th>
+              <th>Total Hours</th>
+            </tr>
+          </thead>
+          <tbody>
+            {exampleStudentList.map((student) => {
+              return (
+                <tr key={student.id}>
+                  <td>{student.LastName}</td>
+                  <td>{student.FirstName}</td>
+                  <td>
+                    <Link to={`/admin/student/${student.id}`}>
+                      {student.id}
+                    </Link>
+                  </td>
+                  <td>{student.MathLvl}</td>
+                  <td>{student.TotalHrs}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
     </div>
   );
 }
